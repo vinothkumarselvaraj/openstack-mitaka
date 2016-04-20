abd013110be7322a8df6920dbd91849671a276ec
vinothkumarselvaraj <vinothkumar6664@me.com>
2016-04-20 12:33:14 +0530

    added package list folder
    package list folder contains the script to install all required packages for openstack setup.

---
+++ mitaka_packages_list/Mitaka-package-list.bash
@@ -0,0 +1,41 @@
+#Mitaka-package-list
+
+#Basic
+apt-get install chrony -y
+apt-get install software-properties-common -y
+add-apt-repository cloud-archive:mitaka 
+apt-get update && apt-get dist-upgrade -y
+apt-get install python-openstackclient -y
+apt-get install mariadb-server python-pymysql -y
+apt-get install rabbitmq-server -y
+apt-get install memcached python-memcache -y
+
+#Keystone
+apt-get install keystone apache2 libapache2-mod-wsgi -y
+
+#Glance
+apt-get install glance -y
+wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -y
+
+#Nova
+apt-get install nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler -y
+apt-get install nova-compute -y
+
+#Neutron
+apt-get install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent -y
+apt-get install neutron-linuxbridge-agent -y
+
+#Horizon
+apt-get install openstack-dashboard -y
+
+#Cinder
+apt-get install cinder-api cinder-scheduler -y
+apt-get install lvm2 -y
+apt-get install cinder-volume -y
+
+#Manila
+apt-get install manila-api manila-scheduler -y
+apt-get install manila-share python-pymysql -y
+
+	#Shared File Systems Option 1: No driver support for share servers management
+	apt-get install lvm2 nfs-kernel-server
